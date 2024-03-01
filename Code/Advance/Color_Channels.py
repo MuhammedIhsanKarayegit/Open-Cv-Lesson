e417c2f6fde0d5fc61eab4414f926d2ce10ad194
MuhammedIhsanKarayegit <104142526+MuhammedIhsanKarayegit@users.noreply.github.com>
2024-03-01 10:53:32 +0300

    Color Channels

---
+++ Code/Advance/Color_Channels.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as  np
+
+img = cv.imread('Documents\\boston.jpg')
+
+b, g, r = cv.split(img)
+
+cv.imshow('Blue', b)
+cv.imshow( 'Green', g)
+cv.imshow( 'Red', r)
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+merged = cv.merge([b,g,r])
+cv.imshow('Merged', merged)
+
+
+cv.waitKey(0)
